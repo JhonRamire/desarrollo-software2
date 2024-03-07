b262664aba43a871f81bd5233a4106f5283a9796
JhonRamire <jhon18a15@gmail.com>
2024-03-07 09:04:45 -0500

    Se adicionaron las funcionalidades faltantes que no me subieron en los commits pasados del html y javaScript

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+function mostrarDatos() {
+	// Obtener los valores de los campos
+	var nombre = document.getElementById("nombre").value;
+	var email = document.getElementById("email").value;
+
+	// Mostrar un mensaje con los datos
+	alert("Nombre: " + nombre + "\nCorreo Electrónico: " + email);
+}
+
+function mostrarDatosP() {
+	// Obtener los valores de los campos
+	var nombreProducto = document.getElementById("nombreProducto").value;
+	var precio = document.getElementById("precio").value;
+	var descripcion = document.getElementById("descripcion").value;
+	var foto = document.getElementById("foto").value; // Aquí puedes realizar un procesamiento adicional para obtener el nombre del archivo
+
+	// Mostrar un mensaje con la información del producto
+	alert(
+		"Nombre del producto: " +
+		nombreProducto +
+		"\nPrecio: $" +
+		precio +
+		"\nDescripción: " +
+		descripcion +
+		"\nFoto: " +
+		foto
+	);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
 	// Seleccionar el boton y agrega un evento de click
 	var miBoton = document.getElementById("miBoton");
@@ -9,4 +38,4 @@ document.addEventListener("DOMContentLoaded", function () {
 	function realizarFuncion() {
 		alert("https://github.com/JhonRamire/desarrollo-software2/tree/jhon");
 	}
-})
+});
