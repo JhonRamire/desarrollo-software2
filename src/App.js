21f98c503bb2a8cc84ebde2ed7e242fea2138d4f
JhonRamire <jhon18a15@gmail.com>
2024-03-08 07:53:28 -0500

    Funcionan las rutas en el app.js (Se va a ir a los diferentes componentes para renderizar la informacion de cada uno de ellos ahi)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 // App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import InfoSection from './components/InfoSection/InfoSection';
 import CategoriesSection from './components/CategoriaSection/CategoriaSection';
 import ProductsSection from './components/ProducSection/ProductSection';
-import Footer from './components/Footer/Footer';
+import ApiSection from './components/ApiSection/ApiSection';
+import FormSection from './components/FormSection/FormSection';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <main>
-        <InfoSection />
-        <CategoriesSection />
-        <ProductsSection />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/info' Component={InfoSection} />
+          <Route path='/categoria' Component={CategoriesSection} />
+          <Route path="/productos" component={ProductsSection} />
+          <Route path="/api" component={ApiSection} />
+          <Route path="/form" component={FormSection} />
+          <Route path="/login" component={Login} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
