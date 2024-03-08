d4483ac21ec9f747f824426258f5caf5f75897c0
JhonRamire <jhon18a15@gmail.com>
2024-03-08 07:37:15 -0500

    Se crearon las rutas para los diferentes componentes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,14 @@ import React from 'react';
 function Navbar() {
     return (
         <nav>
-            <a href="#info">Información</a>
-            <a href="#categoria">Categoría</a>
-            <a href="#productos">Productos</a>
-            <input type="search" />
+            <ul>
+                <li><a href="#info">Información</a></li>
+                <li><a href="#categoria">Categoría</a></li>
+                <li><a href="#productos">Productos</a></li>
+                <li><a href="#api">API</a></li>
+                <li><a href="#form">Formulario</a></li>
+                <li><a href="#login">Iniciar sesión</a></li>
+            </ul>
         </nav>
     );
 }
