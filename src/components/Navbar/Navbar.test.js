6a3efd9019d5396cdc1fa058bf60dfe9f7b44239
JhonRamire <jhon18a15@gmail.com>
2024-03-10 18:44:35 -0500

    Se creo la estructura base para el archivo de test.js del Navbar

---
+++ src/components/Navbar/Navbar.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Navbar from './Navbar'; // Asegúrate de importar el componente correcto
+
+describe('Navbar Component', () => {
+    test('renders Navbar component', () => {
+        // Coloca aquí tu prueba
+    });
+
+    // Agrega más pruebas aquí...
+});
