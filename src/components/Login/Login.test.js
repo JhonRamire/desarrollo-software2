67efc16ef417337a204f5b41c28ec2b1c4d9f123
JhonRamire <jhon18a15@gmail.com>
2024-03-10 18:43:49 -0500

    Se creo la estructura base para el archivo de test.js del Login

---
+++ src/components/Login/Login.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Login from './Login'; // Asegúrate de importar el componente correcto
+
+describe('Login Component', () => {
+    test('renders Login component', () => {
+        // Coloca aquí tu prueba
+    });
+
+    // Agrega más pruebas aquí...
+});
