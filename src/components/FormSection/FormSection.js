4509599ecdb4d840314b7c31a87249418942e0af
JhonRamire <jhon18a15@gmail.com>
2024-03-10 16:43:43 -0500

    Se realiazaron los ajustes de las funcionalidades que ya tenia en el proyecto ahora funcionales en react

--- src/components/FormSection/FormSection.js
+++ src/components/FormSection/FormSection.js
@@ -1,85 +1,75 @@
 // FormSection.js
-import React from 'react';
+import React, { useState } from 'react';
 
 function FormSection() {
+    const [nombre, setNombre] = useState('');
+    const [email, setEmail] = useState('');
+    const [nombreProducto, setNombreProducto] = useState('');
+    const [precio, setPrecio] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [foto, setFoto] = useState('');
+
+    const mostrarGit = () => {
+        alert("https://github.com/JhonRamire/desarrollo-software2/tree/jhon");
+    }
+
+    const mostrarDatos = () => {
+        // Mostrar un mensaje con los datos
+        alert("Nombre: " + nombre + "\nCorreo Electrónico: " + email);
+    }
+
+    const mostrarDatosP = () => {
+        // Aquí puedes realizar cualquier procesamiento adicional necesario para el nombre del archivo de la foto
+
+        // Mostrar un mensaje con la información del producto
+        alert(
+            "Nombre del producto: " +
+            nombreProducto +
+            "\nPrecio: $" +
+            precio +
+            "\nDescripción: " +
+            descripcion +
+            "\nFoto: " +
+            foto
+        );
+    }
+
     return (
         <div>
-            <h2>Funcionalidadjs</h2>
-            <button id="miBoton">click aqui para ver mi repositorio</button>
+            <h2>Funcionalidad JS</h2>
+            <button id="miBoton" onClick={mostrarGit}>Click aquí para ver mi repositorio</button>
             <form id="miFormulario">
-                <label for="nombre">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" />
+                <label htmlFor="nombre">Nombre:</label>
+                <input type="text" id="nombre" name="nombre" onChange={(e) => setNombre(e.target.value)} />
                 <br />
 
-                <label for="email">Correo Electrónico:</label>
-                <input type="email" id="email" name="email" />
+                <label htmlFor="email">Correo Electrónico:</label>
+                <input type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)} />
                 <br />
 
-                <button type="button" onclick="mostrarDatos()">Enviar</button>
+                <button type="button" onClick={mostrarDatos}>Enviar</button>
             </form>
             <form id="miFormularioProducto">
-                <label for="nombreProducto">Nombre del producto:</label>
-                <input type="text" id="nombreProducto" name="nombreProducto" required />
+                <label htmlFor="nombreProducto">Nombre del producto:</label>
+                <input type="text" id="nombreProducto" name="nombreProducto" required onChange={(e) => setNombreProducto(e.target.value)} />
                 <br />
 
-                <label for="price">Precio del producto:</label>
-                <input type="number" id="precio" name="precio" />
+                <label htmlFor="price">Precio del producto:</label>
+                <input type="number" id="precio" name="precio" onChange={(e) => setPrecio(e.target.value)} />
                 <br />
 
-                <label for="descripcion">Descripción del producto:</label>
-                <textarea id="descripcion" name="descripcion"></textarea>
+                <label htmlFor="descripcion">Descripción del producto:</label>
+                <textarea id="descripcion" name="descripcion" onChange={(e) => setDescripcion(e.target.value)}></textarea>
                 <br />
 
-                <label for="foto">Subir imagen del producto:</label>
-                <input type="file" id="foto" name="foto" />
+                <label htmlFor="foto">Subir imagen del producto:</label>
+                <input type="file" id="foto" name="foto" onChange={(e) => setFoto(e.target.value)} />
                 <br />
 
-                <button type="button" onclick="mostrarDatosP()">Enviar</button>
+                <button type="button" onClick={mostrarDatosP}>Enviar</button>
             </form>
         </div>
     );
 }
 
 export default FormSection;
-
-// function mostrarDatos() {
-// 	// Obtener los valores de los campos
-// 	var nombre = document.getElementById("nombre").value;
-// 	var email = document.getElementById("email").value;
-
-// 	// Mostrar un mensaje con los datos
-// 	alert("Nombre: " + nombre + "\nCorreo Electrónico: " + email);
-// }
-
-// function mostrarDatosP() {
-// 	// Obtener los valores de los campos
-// 	var nombreProducto = document.getElementById("nombreProducto").value;
-// 	var precio = document.getElementById("precio").value;
-// 	var descripcion = document.getElementById("descripcion").value;
-// 	var foto = document.getElementById("foto").value; // Aquí puedes realizar un procesamiento adicional para obtener el nombre del archivo
-
-// 	// Mostrar un mensaje con la información del producto
-// 	alert(
-// 		"Nombre del producto: " +
-// 		nombreProducto +
-// 		"\nPrecio: $" +
-// 		precio +
-// 		"\nDescripción: " +
-// 		descripcion +
-// 		"\nFoto: " +
-// 		foto
-// 	);
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-// 	// Seleccionar el boton y agrega un evento de click
-// 	var miBoton = document.getElementById("miBoton");
-
-// 	miBoton.addEventListener("click", function () {
-// 		realizarFuncion();
-// 	});
-
-// 	function realizarFuncion() {
-// 		alert("https://github.com/JhonRamire/desarrollo-software2/tree/jhon");
-// 	}
-// });
