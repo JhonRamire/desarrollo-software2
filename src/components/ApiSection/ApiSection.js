b6a75eb00ef7e26730bb19c6a9ca1dd6ca2e087b
JhonRamire <jhon18a15@gmail.com>
2024-03-10 17:52:12 -0500

    Se conecto a la api public de una serie que me gusta mucho rick y morty se trajo los personajes usando fetch

--- src/components/ApiSection/ApiSection.js
+++ src/components/ApiSection/ApiSection.js
@@ -1,13 +1,27 @@
-// InfoSection.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ApiSection() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character')
+            .then(response => response.json())
+            .then(data => {
+                setCharacters(data.results);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
     return (
-        <section class="info" id="info">
-            <h1>Api</h1>
-            <p>
-                Lorem
-            </p>
+        <section className="info" id="info">
+            <h1>API - Rick and Morty Characters</h1>
+            <ul>
+                {characters.map(character => (
+                    <li key={character.id}>{character.name}</li>
+                ))}
+            </ul>
         </section>
     );
 }
