6ea626a264f22e31307b00d7e33926e0eb5171f0
JhonRamire <jhon18a15@gmail.com>
2024-03-10 18:41:38 -0500

    Se creo la estructura base para el archivo de test.js del FormSection

---
+++ src/components/FormSection/FormSection.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import FormSection from './FormSection'; // Asegúrate de importar el componente correcto
+
+describe('FormSection Component', () => {
+    test('renders FormSection component', () => {
+        // Coloca aquí tu prueba
+    });
+
+    // Agrega más pruebas aquí...
+});
