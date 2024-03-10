73e414b0d519c0ccc35c71275dc104720b61b570
JhonRamire <jhon18a15@gmail.com>
2024-03-10 18:00:42 -0500

    Se realizaron unas pequeñas adiciones en el punto 4. describiendo el proceso de utilizacion de la API

--- src/components/ApiSection/ApiSection.js
+++ src/components/ApiSection/ApiSection.js
@@ -17,6 +17,12 @@ function ApiSection() {
     return (
         <section className="info" id="info">
             <h1>API - Rick and Morty Characters</h1>
+            <p>
+                1. Busque Apis publicas en google. (https://platzi.com/blog/12-api-gratis-para-desarrolladores-frontend/)<br />
+                2. Elegi la de rick y morty y fui donde me redirecciono el link que proporsionaron en el articulo de platzi (https://rickandmortyapi.com/documentation)
+                3. Luego baje hasta donde decia GET que es para obtener los datos que mandan y elegi el de character.
+                4. El codigo de fetch lo saque leyendo el siguiente articulo (https://www.freecodecamp.org/espanol/news/consumiendo-una-api-rest-con-react-js/)
+            </p>
             <ul>
                 {characters.map(character => (
                     <li key={character.id}>{character.name}</li>
