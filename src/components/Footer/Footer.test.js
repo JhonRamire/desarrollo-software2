058565bb81a3df262d9802233798a65741d03759
JhonRamire <jhon18a15@gmail.com>
2024-03-10 18:40:40 -0500

    Se creo la estructura base para el archivo de test.js del footer

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from './Footer'; // Asegúrate de importar el componente correcto
+
+describe('Footer Component', () => {
+    test('renders Footer component', () => {
+        // Coloca aquí tu prueba
+    });
+
+    // Agrega más pruebas aquí...
+});
