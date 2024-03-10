b35b7a4f0c09d2766ccf794fb7647df78a8d9ab9
JhonRamire <jhon18a15@gmail.com>
2024-03-10 18:42:42 -0500

    Se creo la estructura base para el archivo de test.js del InfoSection

---
+++ src/components/InfoSection/InfoSection.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import InfoSection from './InfoSection'; // Asegúrate de importar el componente correcto
+
+describe('InfoSection Component', () => {
+    test('renders InfoSection component', () => {
+        // Coloca aquí tu prueba
+    });
+
+    // Agrega más pruebas aquí...
+});
