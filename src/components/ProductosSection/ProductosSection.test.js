5ec0c018a6a675be210699b412b318409ea3f222
JhonRamire <jhon18a15@gmail.com>
2024-03-10 18:45:21 -0500

    Se creo la estructura base para el archivo de test.js del ProductosSection

---
+++ src/components/ProductosSection/ProductosSection.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ProductosSection from './ProductosSection'; // Asegúrate de importar el componente correcto
+
+describe('ProductosSection Component', () => {
+    test('renders ProductosSection component', () => {
+        // Coloca aquí tu prueba
+    });
+
+    // Agrega más pruebas aquí...
+});
