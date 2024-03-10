e3c4ed2950bbb9737674761617a29602a393725f
JhonRamire <jhon18a15@gmail.com>
2024-03-10 18:39:18 -0500

    Se crearon los archivos para los test de los diferentes componentes

---
+++ src/components/ApiSection/ApiSection.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ApiSection from './ApiSection';
+
+describe('ApiSection Component', () => {
+    test('renders ApiSection component', () => {
+        // Coloca aquí tu prueba
+    });
+
+    // Agrega más pruebas aquí...
+});
---
+++ src/components/CategoriaSection/CategoriaSection.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CategoriaSection from './CategoriaSection';
+
+describe('CategoriaSection Component', () => {
+    test('renders CategoriaSection component', () => {
+        // Coloca aquí tu prueba
+    });
+
+    // Agrega más pruebas aquí...
+});
