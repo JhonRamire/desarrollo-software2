4907d610fe3f0b54df5cbb6f85f35435d345f47c
JhonRamire <jhon18a15@gmail.com>
2024-03-10 14:10:17 -0500

    Se cambiaron algunas imagenes de los productos de maletas, se les dio la ruta se les cambio la descripcion y los productos maletas (La estructura quedo bien)

--- src/components/ProductosSection/ProductosSection.js
+++ src/components/ProductosSection/ProductosSection.js
@@ -8,59 +8,36 @@ function ProductosSection() {
             <div>
                 <h3>Producto bolso 1</h3>
                 <img src="./asset/imgProductoBolso00.jpg" alt="" />
-                <p>Esta es una descripcion de maleta producto 1</p>
+                <p>Esta es una descripcion de producto 1 de la categoria bolso</p>
                 <br />
                 <h3>Producto bolso 2</h3>
                 <img src="./asset/imgProductoBolso01.jpg" alt="" />
-                <p>Esta es una descripcion de maleta producto 2</p>
+                <p>Esta es una descripcion de producto 2 de la categoria bolso</p>
                 <br />
                 <h3>Producto bolso 3</h3>
                 <img src="./asset/imgProductoBolso02.jpg" alt="" />
-                <p>Esta es una descripcion de maleta producto 3</p>
+                <p>Esta es una descripcion de producto 3 de la categoria bolso</p>
                 <br />
                 <h3>Producto bolso 4</h3>
                 <img src="./asset/imgProductoBolso03.jpg" alt="" />
-                <p>Esta es una descripcion de maleta producto 4</p>
+                <p>Esta es una descripcion de producto 4 de la categoria bolso</p>
             </div>
             <div>
-                <h3>Maleta producto2</h3>
-                <img src="./asset/imagenMaletas1.jpg" alt="" />
-                <p>esta es una descripcion de la maelta producto 2</p>
-            </div>
-
-            <div>
-                <h3>Maleta producto3</h3>
-                <img src="./asset/imagenCategoriaMaletas5.jpg" alt="" />
-                <p>esta es una descripcion de la maelta producto 3</p>
-            </div>
-
-            <div>
-                <h3>Bolso producto1</h3>
-                <img src="./asset/imagenCategoriaBolsos3.jpg" alt="" />
-                <p>esta es una descripcion del bolso producto 1</p>
-            </div>
-
-            <div>
-                <h3>Accesorio producto1</h3>
-                <img src="./asset/imagenCategoriaAccesorios1.jpg" alt="" />
-                <p>esta es una descripcion del accesorio producto 1</p>
-            </div>
-
-            <div>
-                <h3>Accesorio producto2</h3>
-                <img src="./asset/imagenCategoriaAccesorios2.jpg" alt="" />
-                <p>esta es una descripcion del accesorio producto 2</p>
-            </div>
-
-            <div>
-                <h3>Accesorio producto3</h3>
-                <img src="./asset/imagenCategoriaAccesorios3.jpg" alt="" />
-                <p>esta es una descripcion del accesorio producto 3</p>
-            </div>
-            <div>
-                <h3>Accesorio producto4</h3>
-                <img src="./asset/imagenCategoriaAccesorios4.jpg" alt="" />
-                <p>esta es una descripcion del accesorio producto 4</p>
+                <h3>Producto maleta 1</h3>
+                <img src="./asset/imgProductoMaleta00.jpg" alt="" />
+                <p>Esta es una descripcion de producto 1 de la categoria maleta</p>
+                <br />
+                <h3>Producto maleta 2</h3>
+                <img src="./asset/imgProductoMaleta01.jpg" alt="" />
+                <p>Esta es una descripcion de producto 2 de la categoria maleta</p>
+                <br />
+                <h3>Producto maleta 3</h3>
+                <img src="./asset/imgProductoMaleta02.jpg" alt="" />
+                <p>Esta es una descripcion de producto 3 de la categoria maleta</p>
+                <br />
+                <h3>Producto maleta 4</h3>
+                <img src="./asset/imgProductoMaleta00.jpg" alt="" />
+                <p>Esta es una descripcion de producto 4 de la categoria maleta</p>
             </div>
         </section>
     );
